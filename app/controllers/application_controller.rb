7720323457a69062a17a0ed3effbe62be21bb401
bognerth <bognerth@gmail.com>
2014-04-06 12:26:44 +0200

    Noten

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,26 +17,30 @@ class ApplicationController < ActionController::Base
       return @current_user
     end
   end
+
   helper_method :current_lecture
   def current_lecture
-    if params[:lecture]
-      session[:wiki_lecture] = params[:lecture]
-      @current_lecture = params[:lecture]
-    elsif session[:wiki_lecture]
-      @current_lecture = session[:wiki_lecture]
-    elsif @current_user && @current_user.admin?
-      l = Lecture.all 
-      session[:wiki_lecture] = l[0].id
-      @current_lecture = l[0].id
-    else
-      redirect_to ENV['ROOT_DOMAIN'], alert: "Der Kurs ist nicht angegeben." 
+    unless request.format == "application/json"
+      if params[:lecture]
+        session[:wiki_lecture] = params[:lecture]
+        @current_lecture = params[:lecture]
+      elsif session[:wiki_lecture]
+        @current_lecture = session[:wiki_lecture]
+      elsif @current_user && @current_user.admin?
+        l = Lecture.all 
+        session[:wiki_lecture] = l[0].id
+        @current_lecture = l[0].id
+      else
+        redirect_to ENV['ROOT_DOMAIN'], alert: "Der Kurs ist nicht angegeben." 
+      end
     end
   end
+
   def authorize
     unless request.format == "application/json"
       if @current_user.nil?
         #Rails.logger.debug "Current_user: #{@current_user.inspect}Dokumente Autorisierung fehlgeschlagen. Loggen Sie sich mit der G16-Kennung und Passwort ein. User: #{session[:user_id]}" 
-        redirect_to ENV['ROOT_DOMAIN'], alert: "Autorisierung fehlgeschlagen. Meckern Sie mit dem Programmierer." 
+        redirect_to ENV['ROOT_DOMAIN'], alert: "Autorisierung fehlgeschlagen." 
       end
     end
   end
